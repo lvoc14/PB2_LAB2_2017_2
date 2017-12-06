f78cdf7e4ee0f3b905ff323d7aec42c73f36a28d
leonardo victor <leonardovictor5@hotmail.com>
2017-12-06 19:46:26 -0300

    Criando todas as classes

---
+++ WebAppProva2bLab2lvoc14/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppProva2bLab2lvoc14.MvcApplication" Language="C#" %>
---
+++ WebAppProva2bLab2lvoc14/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppProva2bLab2lvoc14.Startup))]
+namespace WebAppProva2bLab2lvoc14
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
