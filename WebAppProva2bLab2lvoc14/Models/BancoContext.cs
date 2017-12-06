5ee52c818e6e380c21aae429b017834ddf2ac050
leonardo victor <leonardovictor5@hotmail.com>
2017-12-06 20:14:46 -0300

    mapeando as pluralidades

--- WebAppProva2bLab2lvoc14/Models/BancoContext.cs
+++ WebAppProva2bLab2lvoc14/Models/BancoContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -8,5 +9,14 @@ namespace WebAppProva2bLab2lvoc14.Models
 {
     public class BancoContext:DbContext
     {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Removendo as convençõpes de puralidade um para muitos e muitos para muitos
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //Mapenado todas as classes datatime
+            molderbuilder.properties<DateTime>.configure(p => p.HasMaxLength(200));
+        }
     }
 }
